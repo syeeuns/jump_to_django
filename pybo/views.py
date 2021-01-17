cd4895f7cc909efe7758843f541a29f6a20a9257
syeeuns <syeeuns@gmail.com>
2021-01-17 19:23:38 +0900

    make question, answer

---
+++ pybo/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Question
+
+def index(request):
+    question_list = Question.objects.order_by('-create_date') # 작성일시 역순으로 조회
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
